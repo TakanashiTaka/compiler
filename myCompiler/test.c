a5513ce3a2f4cf2fb0fa4d8d44cf2b4127c093da
TakanashiTaka <70187637+TakanashiTaka@users.noreply.github.com>
2021-12-28 18:48:11 +0800

    lab06 v1.1

--- myCompiler/test.c
+++ myCompiler/test.c
@@ -1,13 +1,8 @@
 
 int main() {
     int b=2;
-    while(b<20){
-        b=b+1;
-        if(b==4){
-            b=b+2;
-            continue;
-        }
-        b=b+1;
+    while(b){
+        b=b-1;
     }
     return 0;
 }
